174271b10eac2c479263b1a073a6b2625b409b1d
jerome-belloir <belloirj@gmail.com>
2021-12-29 09:08:57 +0100

    Add points

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,9 @@ import Projection from 'ol/proj/Projection';
 import Style from 'ol/style/Style';
 import Icon from 'ol/style/Icon';
 import Point from 'ol/geom/Point';
+import VectorSource from 'ol/source/Vector';
+import { fromLonLat } from 'ol/proj';
+import { toLonLat } from 'ol/proj';
 
 @Component({
   selector: 'my-app',
@@ -22,21 +25,58 @@ export class AppComponent implements AfterViewInit {
   map: Map;
 
   constructor() {
+    // Points
+      const paris = new Feature({
+        geometry: new Point(fromLonLat([2.353, 48.8566])),
+        name: 'Null Island',
+      });
+
+      const rennes = new Feature({
+        geometry: new Point(fromLonLat([-1.675,48.11])),
+        name: 'Null Island',
+      });
+
+    // Style 
+      paris.setStyle(
+        new Style({
+          image: new Icon({
+            src: 'https://openlayers.org/en/latest/examples/data/bigdot.png',
+            scale: 0.1,
+          }),
+        })
+      );
+
+      rennes.setStyle(
+        new Style({
+          image: new Icon({
+            src: 'https://openlayers.org/en/latest/examples/data/bigdot.png',
+            scale: 0.1,
+          }),
+        })
+      );
+
+    const vectorSource = new VectorSource({ features: [paris, rennes] });
+    const vectorLayer = new VectorLayer({ source: vectorSource });
+
     this.map = new Map({
       layers: [
         new TileLayer({
           source: new OSM(),
         }),
+        vectorLayer,
       ],
       view: new View({
-        center: [174.76, -37.18],
-        zoom: 7, //Initial Zoom Level
+        center: fromLonLat([2.353, 48.8566]),
+        zoom: 5, //Initial Zoom Level
       }),
     });
   }
 
   ngAfterViewInit() {
     this.map.setTarget('map');
-    this.map.addLayer.Point.Marker([0, 0]);
+
+    this.map.on('singleclick', function (evt) {
+      window.alert(toLonLat(evt.coordinate));
+    });
   }
 }
